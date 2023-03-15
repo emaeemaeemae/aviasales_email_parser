1c5e22f7ac9ab5a46370396927b02ef4d250c224
mysin.i <mysin_id@mail.ru>
2023-03-15 12:08:29 +0300

    add custom errors

---
+++ errors.py
@@ -0,0 +1,22 @@
+class GetEmailListError(Exception):
+    pass
+
+
+class EmailListDecodeError(Exception):
+    pass
+
+
+class GetEmailHeaderError(Exception):
+    pass
+
+
+class EmailMessageDecodeError(Exception):
+    pass
+
+
+class GetCityError(Exception):
+    pass
+
+
+class GetPriceError(Exception):
+    pass
