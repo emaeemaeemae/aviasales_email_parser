ac3ce8176b48cd4f29e69fd58659f4e16cdc7eb9
mysin.i <mysin_id@mail.ru>
2023-03-15 13:22:41 +0300

    add Notifier class

---
+++ notify.py
@@ -0,0 +1,24 @@
+import os
+import urllib3
+import config
+
+
+class Notifier:
+    def __init__(self):
+        self.bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+
+    def send_notify(self, lst):
+        http = urllib3.PoolManager()
+        api = config.TELEGRAM_BOT_API
+        chat = config.CHAT_ID
+        text = self.create_message(lst)
+        http.request('get',
+                     f'{api}{self.bot_token}/'
+                     f'sendMessage?chat_id={chat}&text={text}')
+
+    @staticmethod
+    def create_message(lst):
+        text = 'Следующие предложения соответствуют критериям:\n'
+        for city, price in lst:
+            text += f'полет в {city} за {price} рублей'
+        return text
